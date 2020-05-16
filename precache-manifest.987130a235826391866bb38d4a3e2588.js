9cd51b48696cb5d6777d8fdc98cb935faad21a58
Elaine Tan <elainet@comp.nus.edu.sg>
2020-05-16 17:26:10 +0800

    Updates

---
+++ precache-manifest.987130a235826391866bb38d4a3e2588.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658792732ffbf2061ffe792075634ffd",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "3159957c41d67f57087c",
+    "url": "/to-do-list/static/css/main.2cdddc22.chunk.css"
+  },
+  {
+    "revision": "4548c4a13c9c437d4fe7",
+    "url": "/to-do-list/static/js/2.6dcc3260.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.6dcc3260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3159957c41d67f57087c",
+    "url": "/to-do-list/static/js/main.d7ad1947.chunk.js"
+  },
+  {
+    "revision": "1a1445010e473b6cbbe1",
+    "url": "/to-do-list/static/js/runtime-main.ff26fdc7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
